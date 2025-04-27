a317309ff483462f2b21c91a01bab39c91650106
Raul A. Gonzalez <info@rgonzalez.tech>
2025-04-27 15:48:32 -0700

    Add container remove instructions

--- run_image.sh
+++ run_image.sh
@@ -1,2 +1,6 @@
 # docker build . -t impulse-ticket
 docker run -d --name impulse-ticket -p 8080:8000 -v "$(pwd)/data":/app/data --restart unless-stopped impulse-ticket
+
+# Then, you can remove it
+# docker container stop impulse-ticket
+# docker container rm impulse-ticket
